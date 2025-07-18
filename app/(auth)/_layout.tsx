1786d2dcc54e633b9007c0f347a93629c54c1f9f
Sardor-web-dev <dzamolovsardor5@gmail.com>
2025-07-19 02:58:14 +0500

    fixed errors

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,23 +1,45 @@
-import {Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, Text, View} from 'react-native'
-import {Redirect, Slot} from "expo-router";
-import {images} from "@/constants";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
+import { Redirect, Slot } from "expo-router";
+import { images } from "@/constants";
 import useAuthStore from "@/store/auth.store";
 
 export default function AuthLayout() {
-    const {isAuthentificated} = useAuthStore();
+  const { isAuthentificated } = useAuthStore();
 
-    if (isAuthentificated) return <Redirect href="/"/>
-    return (
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? 'padding' : "height"}>
-            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
-                <View className="w-full relative" style={{height: Dimensions.get('screen').height / 2.25}}>
-                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg"
-                                     resizeMode='stretch'/>
-                    <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10"
-                           resizeMode='stretch'/>
-                </View>
-                <Slot/>
-            </ScrollView>
-        </KeyboardAvoidingView>
-    )
+  if (isAuthentificated) return <Redirect href="/" />;
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+            resizeMode="stretch"
+          />
+        </View>
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
